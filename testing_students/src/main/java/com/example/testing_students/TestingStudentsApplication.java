a0649ff11fbf4d476941ac75751a9670409a0347
Maxim Kazakov <kazakov.java@gmail.com>
2022-04-29 09:59:05 +0300

    Initial commit

---
+++ testing_students/src/main/java/com/example/testing_students/TestingStudentsApplication.java
@@ -0,0 +1,13 @@
+package com.example.testing_students;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingStudentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingStudentsApplication.class, args);
+	}
+
+}
