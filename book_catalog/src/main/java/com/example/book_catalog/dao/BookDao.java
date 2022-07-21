7d49cadd3a2873cb58bda5f0150a7f77f24c2e43
m-c-kazakov <66155572+m-c-kazakov@users.noreply.github.com>
2022-07-21 15:09:12 +0300

    Delete book_catalog directory

--- book_catalog/src/main/java/com/example/book_catalog/dao/BookDao.java
+++
@@ -1,7 +0,0 @@
-package com.example.book_catalog.dao;
-
-import com.example.book_catalog.domain.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookDao extends JpaRepository<Book, Long> {
-}
