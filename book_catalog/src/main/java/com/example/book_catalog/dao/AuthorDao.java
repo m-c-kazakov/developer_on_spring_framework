d5ac74d8a435a5bb9d977595f20edc5a57366fe3
m-c-kazakov <66155572+m-c-kazakov@users.noreply.github.com>
2022-07-21 15:12:57 +0300

    Delete book_catalog directory

--- book_catalog/src/main/java/com/example/book_catalog/dao/AuthorDao.java
+++
@@ -1,8 +0,0 @@
-package com.example.book_catalog.dao;
-
-import com.example.book_catalog.domain.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorDao extends JpaRepository<Author, Long> {
-
-}
--- book_catalog/src/main/java/com/example/book_catalog/dao/BookCommentDao.java
+++
@@ -1,7 +0,0 @@
-package com.example.book_catalog.dao;
-
-import com.example.book_catalog.domain.BookComment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookCommentDao extends JpaRepository<BookComment, Long> {
-}
