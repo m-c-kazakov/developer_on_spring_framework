5d41c7e9afb006e1f197c8a7bf1abf75ebf475bf
m-c-kazakov <66155572+m-c-kazakov@users.noreply.github.com>
2022-07-21 15:11:01 +0300

    Delete book_catalog directory

--- book_catalog/src/main/java/com/example/book_catalog/dao/GenreDao.java
+++
@@ -1,7 +0,0 @@
-package com.example.book_catalog.dao;
-
-import com.example.book_catalog.domain.Genre;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GenreDao extends JpaRepository<Genre, Long> {
-}
